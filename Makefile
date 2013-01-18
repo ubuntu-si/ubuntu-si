4dda171c8e0f1a351da761b243bfc1897170ee9f
Janez Troha <janez.troha@gmail.com>
2013-01-18 13:29:58 +0100

    Fix builder add irc

--- Makefile
+++ Makefile
@@ -3,26 +3,18 @@ deb64:
 deb32:
 	cd si-ubuntu-defaults && dpkg-buildpackage -d -us -uc -F -ai386 && cd .. && mv si-ubuntu-defaults_* dist32
 iso64:
-	ubuntu-defaults-image --release precise --arch amd64 --package dist/si-ubuntu-defaults_*_all.deb --components main,restricted,universe
+	cd dist/ && ubuntu-defaults-image --release precise --arch amd64 --package si-ubuntu-defaults_*_all.deb --components main,restricted,universe
 iso32:
-	ubuntu-defaults-image --release precise --arch i386 --package dist32/si-ubuntu-defaults_*_all.deb --components main,restricted,universe
+	cd dist32/ && ubuntu-defaults-image --release precise --arch i386 --package si-ubuntu-defaults_*_all.deb --components main,restricted,universe
 test:
 	kvm -m 1024 -cdrom binary.hybrid.iso -boot d 
 
 dist64:
-	mv binary-hybrid.iso dist/
-	cd dist/ && buildtorrent binary-hybrid.iso -a "udp://tracker.publicbt.com:80" -w "http://185.14.186.232/dist/binary-hybrid.iso" binary-hybrid.iso.torrent
-	cd dist/ && zsyncmake -C -e binary-hybrid.iso
+	buildtorrent binary-hybrid.iso -a "udp://tracker.publicbt.com:80" -w "http://185.14.186.232/dist/binary-hybrid.iso" binary-hybrid.iso.torrent
+	zsyncmake -C -e binary-hybrid.iso
 
 dist32:
-	mv binary-hybrid.iso dist32/
-	cd dist32/ && buildtorrent binary-hybrid.iso -a "udp://tracker.publicbt.com:80" -w "http://185.14.186.232/dist32/binary-hybrid.iso" binary-hybrid.iso.torrent
-	cd dist32/ && zsyncmake -C -e binary-hybrid.iso
-
-build32:
-	deb32 iso32 dist32
-
-build:
-	deb iso dist
+	buildtorrent binary-hybrid.iso -a "udp://tracker.publicbt.com:80" -w "http://185.14.186.232/dist32/binary-hybrid.iso" binary-hybrid.iso.torrent
+	zsyncmake -C -e binary-hybrid.iso
 
 .PHONY: deb deb32 iso iso32 test dist dist32
--- webhook_builder.py
+++ webhook_builder.py
@@ -3,7 +3,9 @@
 
 import threading
 import logging
-import os
+from pastebin import PastebinAPI
+from Queue import Queue
+from irc import Bot
 
 try:
     from flask import Flask, render_template, request
@@ -20,72 +22,129 @@ logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-10s) %(message)s',
                     )
 app = Flask(__name__)
+zaposlitve = Queue()
+aktivno = None
+
+class Jobber(threading.Thread):
+
+    def run(self):
+        while True:
+            delo = zaposlitve.get()
+            delo.handler = zaposlitve
+            aktivno = delo
+            delo.start()
+job_queue = Jobber()
+job_queue.start()
 
 
-def build_all(flavor):
-    #git_dir = os.path.dirname(os.path.abspath(__file__))
+class IRCBOT(Bot):
 
-    git = envoy.run("git pull")
+    def irc_notify(self, thread, point, text=""):
+        url = ""
+        if len(text) > 1:
+            url = PastebinAPI().paste("94c4c6e3faf2271b9175bb7601738b79", text)
+        for chan in self.channels:
+            if len(url) > 1:
+                msg = "Napaka pri gradnji > {0}: {1} {2}".format(thread.encode("utf-8"), point.encode("utf-8"), url.encode("utf-8"))
+            else:
+                msg = "{0}: {1}".format(thread.encode("utf-8"), point.encode("utf-8"))
+            print msg
+            self.msg(chan, msg)
 
-    logging.debug(git.std_out)
-    if git.status_code is 0:
-
-        deb = envoy.run("make deb" + flavor)
-        logging.debug(deb.std_out)
-
-        if deb.status_code is 0:
-            iso = envoy.run("make iso" + flavor)
-            logging.debug(iso.std_out)
-
-            dist = envoy.run("make dist" + flavor)
-            logging.debug(dist.std_out)
-
-
-def build_amd64():
-    logging.debug('Starting')
-    build_all("64")
-    logging.debug('Exiting')
-
-build_amd64_handle = threading.Thread(name='build_amd64', target=build_amd64)
-
-
-def build_i386():
-    logging.debug('Starting')
-    build_all("32")
-    logging.debug('Exiting')
-
-build_i386_handle = threading.Thread(name='build_i386', target=build_i386)
-
-
-@app.route("/")
-def status():
-    return render_template('status.html',
-                           status_amd64={
-                               "alive": build_amd64_handle.isAlive(),
-                               "history": build_amd64_handle.isAlive(),
-                               "checks": build_amd64_handle.isAlive(),
-                           },
-                           status_i386={
-                               "alive": build_i386_handle.isAlive(),
-                               "history": build_i386_handle.isAlive(),
-                               "checks": build_i386_handle.isAlive(),
-                           })
+    def dispatch(self, origin, args):
+        cmd = "{0}".format(args[0]).encode("utf-8")
+        if '.isostatus' in cmd:
+            if zaposlitve.empty():
+                self.msg(origin.sender, 'Mirujem')
+            else:
+                self.msg(origin.sender, 'Delam')
+                for res in self.history:
+                    self.msg(origin.sender, res)
+        if '.isobuild32' in cmd and "dz0ny" in origin.nick:
+            new_build = Delo('32')
+            zaposlitve.put(new_build)
+            self.msg(origin.sender, 'Začel z izgradnjo 32bit iso')
+        if '.isobuild64' in cmd and "dz0ny" in origin.nick:
+            new_build = Delo('64')
+            zaposlitve.put(new_build)
+            self.msg(origin.sender, 'Začel z izgradnjo 64bit iso')
+bot = IRCBOT('UBBB-iso', "UBBB-iso", ['#ubuntu-si2'])
 
 
-@app.route("/web_hook", methods=['POST'])
-def web_hook():
-    if request.method == 'POST':
+class BOTThread(threading.Thread):
+    """BOTThread"""
+    def run(self):
+        bot.run('irc.freenode.net')
+BOTThread().start()
+
+
+class Delo(threading.Thread):
+
+    """Delo je abstrakcija make build zadeve"""
+    def __init__(self, flavor):
+        self.flavor = flavor
+        self.history = []
+        threading.Thread.__init__(self)
+        self.name = "Delo-build-" + flavor
+
+    def run(self):
+        self.build_all()
+        return
+
+    def check_and_report(self, cmd):
+        if cmd.status_code is 0:
+            self.history.append(u"Command: {0} ✓".format(" ".join(cmd.command)))
+            return True
+        else:
+            bot.irc_notify(self.getName(), " ".join(cmd.command), 'Napaka: {0}\nLog: {1}'.format(cmd.std_err, cmd.std_out))
+            self.history.append(u"Command: {0} ✗".format(" ".join(cmd.command)))
+            return False
+
+    def build_all(self):
+
+        git = envoy.run("git pull")
+        if self.check_and_report(git):
+            deb = envoy.run("make deb" + self.flavor)
+            if self.check_and_report(deb):
+                iso = envoy.run("make iso" + self.flavor)
+                if self.check_and_report(iso):
+                    dist = envoy.run("make dist" + self.flavor)
+                    self.check_and_report(dist)
+
+        for res in self.history:
+            bot.irc_notify(self.getName(), res)
+
+        if self.handler:
+            self.handler.task_done()
+
+# @app.route("/")
+# def status():
+#     return render_template('status.html',
+#                            status_amd64={
+#                                "alive": build_amd64_handle.isAlive(),
+#                                "history": build_amd64_handle.isAlive(),
+#                                "checks": build_amd64_handle.isAlive(),
+#                            },
+#                            status_i386={
+#                                "alive": build_i386_handle.isAlive(),
+#                                "history": build_i386_handle.isAlive(),
+#                                "checks": build_i386_handle.isAlive(),
+#                            })
+
+
+@app.route("/web_hook/<flavor>", methods=['GET', 'POST'])
+def web_hook(flavor):
+    if flavor in ["64", "32"]:
+        logging.debug(flavor)
         if request.remote_addr in ["207.97.227.253", "50.57.128.197",
-                                   "108.171.174.178", "50.57.231.61"]:
-            logging.debug(request.form["payload"])
-            if not build_i386_handle.isAlive():
-                try:
-                    build_i386_handle.start()
-                except Exception, e:
-                    logging.debug(e)
-                    build_i386_handle.start_new_thread(name='build_i386', target=build_i386)
-                    build_i386_handle.start()
+                                   "108.171.174.178", "50.57.231.61",
+                                   "127.0.0.1"]:
+            if request.form["payload"]:
+                logging.debug(request.form["payload"])
+            new_build = Delo(flavor)
+            zaposlitve.put(new_build)
             return "OK"
+    return "FAIL"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
