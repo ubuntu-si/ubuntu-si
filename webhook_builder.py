3c192a0ba884a7205d15cc2762d7e21a02144e3b
Janez Troha <janez.troha@gmail.com>
2013-01-20 01:05:04 +0100

    Be more verbose

--- webhook_builder.py
+++ webhook_builder.py
@@ -149,7 +149,7 @@ def web_hook(flavor):
                 payload = request.form["payload"]
                 payload = json_decode(payload)
                 try:
-                    msg = payload["head_commit"]["committer"]["username"] + u": " + payload["head_commit"]["message"]
+                    msg = "Nov commit: " + payload["head_commit"]["committer"]["username"] + u": " +  payload["head_commit"]["message"] + "\n" + payload["head_commit"]["url"]  + "\n@" + payload["head_commit"]["timestamp"]
                     print msg
                     for chan in bot.channels:
                         bot.msg(chan, msg)
